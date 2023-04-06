b379bb6a14f0a40c174b80958484baa5a919b257
carlosdaniel <125990061+carlosdanielclark@users.noreply.github.com>
2023-04-06 19:52:15 -0400

    updated project. Modification of folders and filles. View improvements

---
+++ public/scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //Dropdown
+    var drop = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(drop);
+  });
+    
+  //Occult message
+  const occult = document.querySelector('.occult');
+  occult.addEventListener('click', (e)=>{
+    close_sms();
+  });
+
+  setTimeout((name)=>{
+    const sms = document.querySelector('.sms');
+    sms.classList.add('hide');
+  }, 2500);
+  
+  const close_sms = ()=>{
+    const sms = document.querySelector('.sms');
+    sms.classList.add('hide');
+  }
